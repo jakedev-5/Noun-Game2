8e3bd7dcaed5e5f135030c1bace2c17f94f971a7
jakedev-5 <58711816+jakedev-5@users.noreply.github.com>
2020-07-21 18:00:18 -0700

    Life

---
+++ Assets/Scripts/MathGame/PlayerRotate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("q"))
+        {
+            transform.Rotate(Vector3.down * speed * Time.deltaTime);
+        }
+        if (Input.GetKey("e"))
+        {
+            transform.Rotate(Vector3.up * speed * Time.deltaTime);
+        }
+    }
+}
