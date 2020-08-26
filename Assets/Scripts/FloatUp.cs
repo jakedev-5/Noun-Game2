a1359e71f644d8f3e4d3439f268d4b651eb37292
jakedev-5 <58711816+jakedev-5@users.noreply.github.com>
2020-08-26 13:24:18 -0700

    hsertg
    shtttttttttrefshhhh

---
+++ Assets/Scripts/FloatUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatUp : MonoBehaviour
+{
+    
+
+    // Start is alledc before the first frame update
+    void Start()
+    {
+        Vector3 pos = gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pos.y += 1;
+        transform.position = pos;
+        Debug.Log(transform.position);
+    }
+}
